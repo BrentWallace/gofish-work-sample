516d447cc5a884966a267479f318001a0b9bcf36
Brent Wallace <brent@bdw.dev>
2020-08-15 17:07:09 +0000

    Moved custom theme functions to separate theme functions file

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,51 @@ function gofish_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'gofish_pingback_header' );
+
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
+
+// Register the services post type
+function create_posttype() {
+ 
+    register_post_type( 'services',
+        array(
+            'labels' => array(
+                'name' => __( 'Services' ),
+                'singular_name' => __( 'Service' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'services'),
+            'show_in_rest' => true,
+        )
+	);
+	register_post_type( 'awards',
+        array(
+            'labels' => array(
+                'name' => __( 'Awards' ),
+                'singular_name' => __( 'Award' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'awards'),
+            'show_in_rest' => true,
+        )
+    );
+}
+// Hooking up our function to theme setup
+add_action( 'init', 'create_posttype' );
+
+// Function to add classes to <li> in nav menus
+function add_additional_class_on_li($classes, $item, $args) {
+    if(isset($args->add_li_class)) {
+        $classes[] = $args->add_li_class;
+    }
+    return $classes;
+}
+add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
