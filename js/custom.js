246e670050f31e07006768870f4a1ec2372c8db6
Brent Wallace <brent@bdw.dev>
2020-08-17 17:08:33 +0000

    Refactored secondary menu item response script

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,38 @@
+// Menu functions
 var menuCollapsed;
+var primaryMenu = document.querySelector('#menu-primary');
 var secondaryMenu = document.querySelector('#menu-secondary');
 var secondaryMenuItems = secondaryMenu.querySelectorAll('#menu-secondary>*');
 
+// Wrap the last menu item text with a button element
+var lastItem = secondaryMenuItems[secondaryMenuItems.length-1];
+var newButton = document.createElement('button');
+newButton.classList.add('btn', 'btn-outline-secondary', 'btn-oval');
+newButton.innerText = lastItem.innerText;
+secondaryMenu.replaceChild(newButton, lastItem);
+
+// Update the secondaryMenuItem Nodelist with the replaced final element
+secondaryMenuItems = secondaryMenu.querySelectorAll('#menu-secondary>*');
+
+
+// Function to move the secondary menu items into the primary during menu collapse & back again on resize
 var shiftMenu = function () {
     var windowWidth = window.innerWidth;    
     
     var collapseMenu = function () {
         menuCollapsed = true;
-
         for (var i=0; i<secondaryMenuItems.length; i++) {
-            secondaryMenuItems[i].classList.add('appended');
-            document.querySelector('#menu-primary').insertAdjacentElement( 'beforeend', secondaryMenuItems[i] );
+            primaryMenu.insertAdjacentElement( 'beforeend', secondaryMenuItems[i] );
         }
-
-        document.querySelector('#menu-secondary').classList.add('d-none');
+        secondaryMenu.classList.add('d-none');
     }
 
     var revertMenu = function () {
         menuCollapsed = false;
-        var appendedMenuItems = document.querySelectorAll('.appended');
-        
-        for (var i=0; i<appendedMenuItems.length; i++) {
-            appendedMenuItems[i].classList.remove('appended');
-            document.querySelector('#menu-secondary').insertAdjacentElement('beforeend',appendedMenuItems[i]);
+        for (var i=0; i<secondaryMenuItems.length; i++) {
+            secondaryMenu.insertAdjacentElement('beforeend',secondaryMenuItems[i]);
         }
-
-        document.querySelector('#menu-secondary').classList.remove('d-none');
+        secondaryMenu.classList.remove('d-none');
     }
 
     if (windowWidth <= 991 && menuCollapsed) {
@@ -39,5 +46,8 @@ var shiftMenu = function () {
     }
 }
 
+// Run shiftMenu on startup to set appropriate menu location
 window.onload = shiftMenu;
+
+// Run shiftMenu on resize to respond to window changes
 window.onresize = shiftMenu;
