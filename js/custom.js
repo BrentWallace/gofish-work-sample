9c8795a5ec8de40d298d8b1543467e99f964bd32
Brent Wallace <brent@bdw.dev>
2020-08-18 16:37:47 +0000

    Refactored responsive scripts. Added function to add or remove services border classes when required.

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,10 @@
+// Get the window width
+var windowWidth;
+
+var getWindowWidth = function () {
+    windowWidth = window.innerWidth;
+}
+
 // Menu functions
 var menuCollapsed;
 var primaryMenu = document.querySelector('#menu-primary');
@@ -14,40 +21,123 @@ secondaryMenu.replaceChild(newButton, lastItem);
 // Update the secondaryMenuItem Nodelist with the replaced final element
 secondaryMenuItems = secondaryMenu.querySelectorAll('#menu-secondary>*');
 
-
 // Function to move the secondary menu items into the primary during menu collapse & back again on resize
 var shiftMenu = function () {
-    var windowWidth = window.innerWidth;    
-    
     var collapseMenu = function () {
         menuCollapsed = true;
-        for (var i=0; i<secondaryMenuItems.length; i++) {
-            primaryMenu.insertAdjacentElement( 'beforeend', secondaryMenuItems[i] );
-        }
-        secondaryMenu.classList.add('d-none');
+        secondaryMenuItems.forEach( function( element ) {
+            primaryMenu.insertAdjacentElement( 'beforeend', element );
+        });
+        secondaryMenu.classList.add( 'd-none' );
     }
 
     var revertMenu = function () {
         menuCollapsed = false;
-        for (var i=0; i<secondaryMenuItems.length; i++) {
-            secondaryMenu.insertAdjacentElement('beforeend',secondaryMenuItems[i]);
-        }
-        secondaryMenu.classList.remove('d-none');
+        secondaryMenuItems.forEach( function ( element ) {
+            secondaryMenu.insertAdjacentElement( 'beforeend', element );
+        });
+        secondaryMenu.classList.remove( 'd-none' );
     }
 
-    if (windowWidth <= 991 && menuCollapsed) {
+    if ( windowWidth <= 991 && menuCollapsed ) {
         return;
     }
-    if (windowWidth <= 991 && !menuCollapsed) {
+    if ( windowWidth <= 991 && !menuCollapsed ) {
         collapseMenu();
     }
-    if (windowWidth > 991 && menuCollapsed) {
+    if ( windowWidth > 991 && menuCollapsed ) {
         revertMenu();
     }
 }
 
-// Run shiftMenu on startup to set appropriate menu location
-window.onload = shiftMenu;
+// Services section border class assignment loop
+var services = document.querySelectorAll( '.service' );
+var serviceClassesApplied;
+var applyServiceClasses = function () {
+    
+    if ( windowWidth >= 768 && serviceClassesApplied ) {
+        return;
+    }
 
+    if ( windowWidth >= 768 && !serviceClassesApplied ) {
+        services.forEach( function ( element, index, services ) {
+            switch ( index ) {
+                case 0:
+                    element.classList.add( 'service-first' );
+                    break;
+                case 1:
+                    element.classList.add( 'service-top' );
+                    break;
+                case 2:
+                    element.classList.add( 'service-top' );
+                    break;
+                case 3:
+                    element.classList.add( 'service-top-last' );
+                    break;
+                case 4:
+                    element.classList.add( 'service-bottom-first' );
+                    break;
+                case 5:
+                    element.classList.add( 'service-bottom' );
+                    break;
+                case 6:
+                    element.classList.add( 'service-bottom' );
+                    break;
+                case 7:
+                    element.classList.add( 'service-last' );
+                default:
+                    break;
+            }
+        });
+        serviceClassesApplied = true;
+    }
+
+    if ( windowWidth < 768 && !serviceClassesApplied ) {
+        return;
+    }
+
+    if ( windowWidth <768 && serviceClassesApplied ) {
+        services.forEach( function ( element, index, services ) {
+            switch ( index ) {
+                case 0:
+                    element.classList.remove( 'service-first' );
+                    break;
+                case 1:
+                    element.classList.remove( 'service-top' );
+                    break;
+                case 2:
+                    element.classList.remove( 'service-top' );
+                    break;
+                case 3:
+                    element.classList.remove( 'service-top-last' );
+                    break;
+                case 4:
+                    element.classList.remove( 'service-bottom-first' );
+                    break;
+                case 5:
+                    element.classList.remove( 'service-bottom' );
+                    break;
+                case 6:
+                    element.classList.remove( 'service-bottom' );
+                    break;
+                case 7:
+                    element.classList.remove( 'service-last' );
+                default:
+                    break;
+            }
+        });
+        serviceClassesApplied = false;
+    }
+}
+
+// Gather previous functions into one
+var resizeFunction = function () {
+    getWindowWidth();
+    shiftMenu();
+    applyServiceClasses();
+}
+
+// Run functions on startup to set appropriate menu location
+window.onload = resizeFunction;
 // Run shiftMenu on resize to respond to window changes
-window.onresize = shiftMenu;
+window.onresize = resizeFunction;
